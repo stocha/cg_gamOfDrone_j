92af42d9721c1b0690ec600c8ecab49e9d8d5a4e
DFI <dfi@dfidaali.com>
2015-05-23 10:52:14 +0200

    Petittes modif + ajout du control.

--- src/PlayerLib.java
+++ src/PlayerLib.java
@@ -15,8 +15,29 @@ import java.util.Scanner;
  * @author Jahan
  */
 public class PlayerLib {
+    
+    public static class GamePos implements GraphicLib2d.WithCoord{
+        final Point cord=new Point(0,0);
+        
+        @Override
+        public Point cord() {
+            return cord;
+        }
+        
+        public void set(Point c){
+            cord.setLocation(c);
+        }
+    }
+        
+    public static class DroneBase extends GamePos{
 
-    public static class BotBase{                
+    }
+    
+    public static class ZoneBase extends GamePos{
+
+    }    
+
+    public static abstract class BotBase{                
         final Scanner in;
 
         final int ID;
@@ -24,11 +45,14 @@ public class PlayerLib {
         final int P;
         final int Z;
         
+        int _turn_Number;
+        final int[] _turn_scoreControl;
+        
         final int[] _owner;
         final Point[][] _playerDronesCords;
         final Point[] _orders;
         
-        BotBase(InputStream inst){
+        public BotBase(InputStream inst){
             this.in = new Scanner(inst);
             P = in.nextInt(); // number of players in the game (2 to 4 players)
             ID = in.nextInt(); // ID of your player (0, 1, 2, or 3)
@@ -49,29 +73,46 @@ public class PlayerLib {
                 _orders[d]=new Point(20,20);
             }
             
+            _turn_scoreControl=new int[P];
+            
         }
         
-        void readTurn(){
+        abstract void doPrepareOrder();
+            
+        
+        public void readTurn(){
             for (int i = 0; i < Z; i++) {
                 int own=in.nextInt();
-                _owner[i] = own;
+                _owner[i] = own;                
             }            
             
             for (int i = 0; i < P; i++) {
                 for (int j = 0; j < D; j++) {
                     _playerDronesCords[i][j].setLocation(in.nextInt(), in.nextInt());
                 }
+                _turn_scoreControl[i]=0;
             }     
+            
+            for (int i = 0; i < Z; i++) {
+                int n=_owner[i];
+                if(n==-1) continue;
+                _turn_scoreControl[n]++;
+            }
+            
             
             for(int d=0;d<D;d++){
                 _orders[d].setLocation(20,20);
             }            
         }
         
-        void writeOrders(PrintStream out){
+        public void writeOrders(PrintStream out){
+            doPrepareOrder();
+            
             for (int d = 0; d < D; d++) {
                 out.println("" + _orders[d].x + " " + _orders[d].y);
-            }            
+            }           
+            
+            _turn_Number++;
         }
     
     }
